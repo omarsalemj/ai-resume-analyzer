daa7690b93abc5f8c5b6c54bc8a5f98b8e18c7f8
omarsalemj <omarsalemj77@gmail.com>
2026-04-13 21:28:00 +0200

    Edit Navbar Btn

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,10 +4,10 @@ const Navbar = () => {
   return (
     <nav className="navbar">
         <Link to='/'>
-            <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+            <p className="text-xl sm:text-2xl font-bold text-gradient">RESUMIND</p>
         </Link>
-        <Link to='/upload' className="primary-button w-fit">
-            Upload Resume
+        <Link to='/upload' className="primary-button font-bold text-center w-fit">
+            Upload
         </Link>
     </nav>
   )
